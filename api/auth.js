07bb87dd95d1d978dd96d775e00c269c8372fe6e
root <root@oslab.com>
2026-03-29 13:42:58 -0400

    Fix OAuth: remove invalid scope param causing Schwab login loop

--- api/auth.js
+++ api/auth.js
@@ -16,7 +16,6 @@ export default async function handler(req, res) {
       response_type: 'code',
       client_id: process.env.SCHWAB_CLIENT_ID,
       redirect_uri: process.env.SCHWAB_REDIRECT_URI,
-      scope: 'readonly',
     });
     return res.redirect(
       `https://api.schwabapi.com/v1/oauth/authorize?${params}`
